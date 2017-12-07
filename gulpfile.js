b9c9668a95340c3e3e97e05870cfcff83a0d04b2
Pavel Karachev <karachevpavel@gmail.com>
2017-12-07 11:28:38 +0300

    Подготовил к работе

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,184 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
-var mqpacker = require("css-mqpacker");
-var minify = require("gulp-csso");
-var imagemin = require("gulp-imagemin");
-var svgmin = require('gulp-svgmin');
-var svgstore = require("gulp-svgstore");
-var rename = require("gulp-rename");
-var run = require("run-sequence");
-var del = require("del");
-var concat = require('gulp-concat');
-var uglify = require("gulp-uglify");
+// ////////////////////////////////////////////////
+//
+// GULP СБОРКА ДЛЯ ВЕРСТКИ САЙТОВ
+//
+// // //////////////////ho///////////////////////////
 
-gulp.task("style", function() {
-    gulp.src("sass/style.scss")
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(postcss([
-            autoprefixer({browsers: [
-                "last 2 versions"
-            ]}),
-            mqpacker({
-                sort: true
-            })
-        ]))
-        .pipe(gulp.dest("build/css"))
-        .pipe(minify())
-        .pipe(rename("style.min.css"))
-        .pipe(gulp.dest("build/css"))
-        .pipe(server.stream());
-});
+/*----------------------------------------
+	PLUGINS
+----------------------------------------*/
 
-gulp.task("clean", function() {
-    return del("build");
-});
+var gulp = require('gulp'),
+    // РАБОТАЕМ С ПРОИЗВОДИТЕЛЬНОСТЬЮ СБОРКИ
+    gulpLoadPlugins = require('gulp-load-plugins'),
+    $ = gulpLoadPlugins(),
+    gulpIf = require('gulp-if'),
 
-gulp.task('script', function() {
-    return gulp.src('build/js/**.js')
-        .pipe(concat('style.js'))
-        .pipe(gulp.dest('build/js'))
-        .pipe(rename('style.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('build/js'));
-});
+    //sprite png files
+    spritesmith = require('gulp.spritesmith'),
 
-gulp.task("copy", function() {
-    return gulp.src([
-        "fonts/**/*.{woff,woff2}",
-        "img/**",
-        "js/**",
-        "*.html"
-    ], {
-        base: "."
-    })
-        .pipe(gulp.dest("build"));
-});
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    plumber = require('gulp-plumber');
 
-gulp.task("images", function() {
-    return gulp.src("build/img/**/*.{png,jpg,gif}")
-        .pipe(imagemin([
-            imagemin.optipng({optimizationLevel: 3}),
-            imagemin.jpegtran({progressive: true})
-        ]))
-        .pipe(gulp.dest("build/img"));
-});
+/*----------------------------------------
+	PATHS / ПУТИ К ФАЙЛАМ
+----------------------------------------*/
+var paths = require('./gulp/paths');
+console.log($);
 
-gulp.task("symbols", function() {
-    return gulp.src("build/img/icons/*.svg")
-        .pipe(svgmin())
-        .pipe(svgstore({inlineSvg: true}))
-        .pipe(rename("symbols.svg"))
-        .pipe(gulp.dest("build/img"));
-});
+// ПОДКЛЮЧАЕМ ОТДЕЛЬНО TASKS
+function lazyRequireTask(taskName, path, options) {
+    options = options || {};
+    options.taskName = taskName;
+    gulp.task(taskName, function(callback) {
+        var task = require(path).call(this, options);
 
-gulp.task("serve", ["style"], function() {
-    server.init({
-        server: "build/",
-        notify: false,
-        open: true,
-        cors: true,
-        ui: false
+        return task(callback);
     });
+}
 
-    gulp.watch("sass/**/*.{scss,sass}", ["style"]);
-    gulp.watch("*.html").on("change", server.reload);
-    gulp.watch("js/**/*.js",["js"]);
+/*----------------------------------------
+	BROWSER SYNC
+----------------------------------------*/
+
+gulp.task('server', ['sass'], function() {
+    browserSync.init({
+        server: "./build",
+        port: 4400
+    });
 });
 
-gulp.task("build", function(fn) {
-    run("clean", "copy", "style", "script", "images", "symbols", "serve", fn);
+gulp.task('watch-html', ['html'], function () {//pug or html
+    browserSync.reload();
 });
+
+gulp.task('watch-js', ['js'], function () {
+    browserSync.reload();
+});
+
+/*----------------------------------------
+	HTML
+----------------------------------------*/
+
+
+lazyRequireTask('html', './gulp/tasks/html', {
+    src: paths.html.src,
+    dest: paths.html.dest
+});
+
+/*----------------------------------------
+	STYLES
+----------------------------------------*/
+//sass
+lazyRequireTask('sass', './gulp/tasks/sass', {
+    src: 'src/sass/app.scss',
+    dest: paths.sass.dest
+});
+
+
+/*----------------------------------------
+	JS
+----------------------------------------*/
+
+lazyRequireTask('js', './gulp/tasks/js', {
+    src: 'src/js/*.js',
+    dest: paths.js.dest,
+    minDest: paths.js.dest
+});
+
+
+//minify js files
+// lazyRequireTask('js:min', './gulp/tasks/minjs', {
+// 	src: paths.js.minSrc,
+// 	dest: paths.js.minDest
+// });
+
+
+/*----------------------------------------
+	SVG
+----------------------------------------*/
+
+//SVG SPRITE
+lazyRequireTask('svg', './gulp/tasks/svg', {
+    src: paths.svg.src,
+});
+
+
+//SVG SPRITE BASE 64
+lazyRequireTask('svg:base', './gulp/tasks/svgBase', {
+    src: paths.base64.src,
+    dest: paths.base64.dest
+});
+
+/*----------------------------------------
+	SPRITE PNG FILES
+----------------------------------------*/
+gulp.task('sprite', function () {
+    var spriteData = gulp.src('src/images/sprite/*')
+        .pipe(spritesmith({
+                imgName: 'sprite.png',
+                // retinaImgName: 'sprite@2x.png',
+                cssName: '_sprite-png.scss',
+                imgPath:'../images/sprite/sprite.png',
+                padding: 15
+            })
+        );
+
+    var imgStream = spriteData.img
+        .pipe(gulp.dest('build/images/sprite'));
+
+    var cssStream = spriteData.css
+        .pipe(gulp.dest('src/sass/sprite/'));
+
+});
+
+
+gulp.task('jsMinSync', function () {
+    return gulp.src('src/js/minifier/**/*.js')
+        .pipe(plumber())
+        .pipe(gulp.dest('build/js/'))
+        .pipe(browserSync.stream());
+});
+
+/*----------------------------------------
+	MINIFY IMAGES
+----------------------------------------*/
+
+lazyRequireTask('image:min', './gulp/tasks/images', {
+    src: paths.images.src,
+    dest: paths.images.dest
+});
+
+gulp.task('copy', function () {
+    return gulp.src('src/assets/**/*')
+        .pipe(gulp.dest('build/'))
+});
+
+
+gulp.task('watch', function () {
+    // sass
+    gulp.watch('src/sass/**/*.scss', ['sass']);
+    // html
+    gulp.watch(['src/html/**/*.html'], ['watch-html']);
+    // js
+    gulp.watch("src/js/**/*.js", ['watch-js']);
+    // images
+    gulp.watch("src/images/*", ['image:min']);
+    // svg
+    gulp.watch('src/svg/*.svg', ['svg']).on('change', function(event){
+        if (event.type === 'deleted') {
+            $.remember.forget('svg', event.path);
+        }
+    });
+    gulp.watch('src/svg/**/*.svg', ['svg:base']);
+    // png
+    gulp.watch('src/images/sprite/*.png', ['sprite']);
+});
+
+
+gulp.task('default', ['js','svg','server','watch']);
+
+gulp.task('build', ['copy','js','svg', 'html', 'sass', 'svg:base', 'image:min']);
