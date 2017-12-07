bbef6f44132a141e8a05142e4b17c2efc61477c2
Pavel Karachev <pavelkarachev@ymail.com>
2017-12-07 23:37:08 +0300

    Адаптивный хэдер

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,15 @@
+var navMain = document.querySelector('.header-nav');
+var navToggle = document.querySelector('.header__trigger');
 
+navMain.classList.remove('header-nav--no-js');
+
+navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('header-nav--closed')) {
+        navMain.classList.remove('header-nav--closed');
+        navToggle.classList.add('header__trigger--closed');
+    } else {
+        navMain.classList.add('header-nav--closed');
+        navToggle.classList.remove('header__trigger--closed');
+    }
+});
 
